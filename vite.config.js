0e7c20d758c04dd3d20677be8fae306c1862434a
rdly7922-dot <rdly7922@gmail.com>
2026-07-14 06:58:03 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,10 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3000,
-    open: true,
-  },
+  port: 3000,
+  open: true,
+  allowedHosts: true,
+},
   build: {
     outDir:        "dist",
     sourcemap:     true,
@@ -30,13 +31,3 @@ export default defineConfig({
     },
   },
 });
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: true
-  }
-})
